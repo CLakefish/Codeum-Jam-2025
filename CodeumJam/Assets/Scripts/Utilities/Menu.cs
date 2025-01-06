91ab62f6ca142d0141744a2f1417524c6fba33b8
Samhutkinsu <117354925+Samhutkinsu@users.noreply.github.com>
2025-01-06 18:27:30 -0500

    Menu UI
    Added in Menu UI, basic but gets it done, its in the Prefabs folder

--- CodeumJam/Assets/Scripts/Utilities/Menu.cs
+++ CodeumJam/Assets/Scripts/Utilities/Menu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Unity.VisualScripting;
+using TMPro;
 
 public class Menu : MonoBehaviour
 {
@@ -11,23 +12,27 @@ public class Menu : MonoBehaviour
     [SerializeField] private GameObject player;
     public GameObject[] menuPanel;
     [SerializeField] Slider VolumeSlider;
+
+    [Header("Menu Objects")]
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+
+    [Header("Level References")]
+    private string description;
+    private string name;
     // Start is called before the first frame update
     void Start()
     {
-        //I find all panels, which is one
-        /* menuPanel = GameObject.FindGameObjectsWithTag("Menu");
-         menuPanel[0] = GameObject.Find("Menu");
-         menuPanel[1] = GameObject.Find("Options");*/
         Player player = GetComponent<Player>();
+        VolumeSlider.value = Load("Volume");
+        Setup();
+        nameText.text = name;
+        descriptionText.text = description;
         foreach (GameObject panel in menuPanel)
         {
             panel.SetActive(false);
-            //Debug.Log(panel.name);
         }
-
     }
-
-    // Update is called once per frame
     void Update()
     {
         if ((Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.E)))
@@ -36,53 +41,44 @@ public class Menu : MonoBehaviour
         }
         
     }
-
-    /*public void OpenMenu()
-    {
-        menuPanel[menuID].SetActive(true);
-        Time.timeScale = 0.0f;
-    }
-    public void Resume()
-    {
-        menuPanel[menuID].SetActive(false);
-        Time.timeScale = 1.0f;
-    }*/
     public void SwitchtoMenu(int menuID)
     {
         foreach (GameObject panel in menuPanel)
         {
             panel.SetActive(false);
-            //Debug.Log(panel.name);
         }
-
         menuPanel[menuID].SetActive(true);
         player.GetComponent<PlayerInput>().MouseLock = false;
-        //player.GetComponent<Player>().AllowMovement(false);
-        //player.
-        //player.playerComponent.PlayerInput.MouseLock = true;
-
         Time.timeScale = 0.0f;
-
     }
     public void CloseMenu()
     {
+        Save_Float("Volume", VolumeSlider.value);
         foreach (GameObject panel in menuPanel)
         {
             panel.SetActive(false);
-            //Debug.Log(panel.name);
         }
         player.GetComponent<PlayerInput>().MouseLock = true;
-        //player.GetComponent<Player>().AllowMovement(true);
         Time.timeScale = 1.0f;
     }
-    public void ReturntoMenu()
+    private void Setup()
     {
-        Time.timeScale = 1.0f;
-        //Returns to main menu
-        //SceneManager.LoadScene(0);
+        name = LevelManager.Instance.currentLevel.name;
+        description = LevelManager.Instance.currentLevel.Description;
+
     }
     public void ChangeVolume()
     {
         AudioListener.volume = VolumeSlider.value;
+    }
+    private void Save_Float(string name, float value)
+    {
+        PlayerPrefs.SetFloat(name, value);
+        PlayerPrefs.Save();
+    }
+  
+    private float Load(string name)
+    {
+        return PlayerPrefs.GetFloat(name, 0.5f);
     }
 }
