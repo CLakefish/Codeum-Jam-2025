cc693645dca89347652abfeda4be2fab6b5017fe
Samhutkinsu <117354925+Samhutkinsu@users.noreply.github.com>
2025-01-08 18:33:49 -0500

    Started on memu

---
+++ CodeumJam/Assets/Scripts/Utilities/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Refrences")]
+    [SerializeField] GameObject[] menuPanels;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
